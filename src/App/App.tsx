1e5d618092f0e7d62512686b5e70e4c9194773bd
Harsh Kanjariya <hk@Harshs-MacBook-Air.local>
2022-12-09 10:38:13 +0530

    react setup done

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC<React.HTMLProps<any>> = () => (
+    <div className="App">
+        Hello
+    </div>
+
+);
+
+export default App;

f01b238cc2096b6de0b85b755adcdcc4f532e26a
Harsh Kanjariya <harsh.kanjariya@pw.live>
2025-01-17 22:27:24 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import {Header} from "./components/Header.tsx";
+
+function App() {
+  return <div>
+    <Header/>
+  </div>;
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+export function Header() {
+  return <header className="sticky">
+    <div className="text-center">Portfolio</div>
+  </header>;
+}
